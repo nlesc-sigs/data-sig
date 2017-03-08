c086750988c53c9905b18cdac4b7b547b66e039b
arnikz <a.kuzniar@esciencecenter.nl>
2017-03-08 12:20:44 +0100

    Small fixes

--- workshops/sqlite3_udfs/python/udfs.py
+++ workshops/sqlite3_udfs/python/udfs.py
@@ -1,8 +1,16 @@
+from __future__ import print_function
+import sys
 import math
 import numpy as np
 import sqlite3 as sqlt
 
-dbfile = 'test.db'
+
+# test user input
+if len(sys.argv) == 1:
+   print("Usage: %s [dbfile]" % sys.argv[0])
+   sys.exit(2)
+
+dbfile = sys.argv[1]
 
 # user-defined (aggregate) functions (UDFs); https://docs.python.org/2/library/sqlite3.html
 def log(value, base):
@@ -50,18 +58,20 @@ with sqlt.connect(dbfile) as conn:
    conn.create_aggregate('median', 1, Median)
    conn.create_aggregate('mad', 1, Mad)
    cur = conn.cursor()
+   log_base = 2
+   precision = 4
    sql_stmt = """
       SELECT
          COUNT(*) AS n,
-         ROUND(MIN(LOG(abundance, 10)), 4) AS min,
-         ROUND(MAX(LOG(abundance, 10)), 4) AS max,
-         ROUND(AVG(LOG(abundance, 10)), 4) AS mean,
-         ROUND(MEDIAN(LOG(abundance, 10)), 4) AS median,
-         ROUND(STDEV(LOG(abundance, 10)), 4) AS stdev,
-         ROUND(MAD(LOG(abundance, 10)), 4) AS mad
+         ROUND(MIN(LOG(abundance, {0})), {1}) AS min,
+         ROUND(MAX(LOG(abundance, {0})), {1}) AS max,
+         ROUND(AVG(LOG(abundance, {0})), {1}) AS mean,
+         ROUND(MEDIAN(LOG(abundance, {0})), {1}) AS median,
+         ROUND(STDEV(LOG(abundance, {0})), {1}) AS stdev,
+         ROUND(MAD(LOG(abundance, {0})), {1}) AS mad
       FROM
          PROTEIN
-   """
+   """.format(log_base, precision)
 
    cur.execute(sql_stmt)
    row = cur.fetchone()
